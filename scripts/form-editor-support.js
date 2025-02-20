7735704e3c166db01ec5530569b1a40615853ab0
Lovely Mandal <lmandal@Lovelys-MacBook-Pro-2.local>
2025-02-20 16:34:39 +0530

    static html page url

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -389,7 +389,8 @@ function enableAuthoringAssistantExtension() {
   const meta = document.createElement('meta');
   meta.name = 'urn:adobe:aue:config:extensions';
   //meta.content = getAuthoringAssistantUrl();
-  meta.content = 'https://localhost.corp.adobe.com:8013/resources/universal_editor.html';
+  //meta.content = 'https://localhost.corp.adobe.com:8013/resources/universal_editor.html'; 
+  meta.content = 'https://127.0.0.1:8013/resources/form_properties.html'
 
   console.log('Adding meta tag for aem forms authoring assistant extension:', meta.content);
   document.head.appendChild(meta);
