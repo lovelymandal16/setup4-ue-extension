ade00ae89838196974b715932b1bfb9032020192
Lovely Mandal <lmandal@Lovelys-MacBook-Pro-2.local>
2025-03-11 16:09:37 +0530

    ...

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -396,7 +396,7 @@ function enableAuthoringAssistantExtension() {
  // meta.content = 'https://experience.adobe.com/solutions/livecycle-forms-spa/resources/form_properties.html?livecycle-forms-spa_version=PR-519-d27b873d572d6f211f558aa51cb8853f02f1cf4d&repo=author-p98711-e226383-cmstg.adobeaemcloud.com';
  //meta.content = 'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-526-e75b0591ac5a8c042129929553f4d82b9171d25b';
  //meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/form_properties.html?livecycle-forms-spa_version=PR-525-c935c8593260ede4dba01fdf01dab0aec9f1c655';
- meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/form_properties.html?livecycle-forms-spa_version=PR-525-5fc0403af543a5bc17d5cd627c418ead6cf65311';
+ meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-525-5fc0403af543a5bc17d5cd627c418ead6cf65311';
 
  console.log('Adding meta tag for aem forms authoring assistant extension:', meta.content);
   document.head.appendChild(meta);
