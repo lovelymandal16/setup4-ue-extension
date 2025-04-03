a7e0a0a4179e84ac1be84b8745a49a6ad3709dad
Lovely Mandal <155715221+lovelymandal16@users.noreply.github.com>
2025-04-03 23:04:59 +0530

    Update form-editor-support.js

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -395,8 +395,8 @@ function enableAuthoringAssistantExtension() {
   //meta.content = 'https://experience-qa.adobe.com/?livecycle-forms-spa_version=PR-519-d27b873d572d6f211f558aa51cb8853f02f1cf4d&repo=author-p98711-e226383-cmstg.adobeaemcloud.com#/@908978/aem/forms/af/create';
  // meta.content = 'https://experience.adobe.com/solutions/livecycle-forms-spa/resources/form_properties.html?livecycle-forms-spa_version=PR-519-d27b873d572d6f211f558aa51cb8853f02f1cf4d&repo=author-p98711-e226383-cmstg.adobeaemcloud.com';
  //meta.content = 'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-526-e75b0591ac5a8c042129929553f4d82b9171d25b';
- //meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/form_properties.html?livecycle-forms-spa_version=PR-525-c935c8593260ede4dba01fdf01dab0aec9f1c655';
- meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-530-361fc72b927d0f2038da43f1ecd0026ffe898785';
+ meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/form_properties.html?livecycle-forms-spa_version=PR-530-361fc72b927d0f2038da43f1ecd0026ffe898785';
+ //meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-530-361fc72b927d0f2038da43f1ecd0026ffe898785';
 
  console.log('Adding meta tag for aem forms authoring assistant extension:', meta.content);
   document.head.appendChild(meta);
