ae3fab28e870d8df1ce7dc9b81ca5b751bd7c24b
Lovely Mandal <155715221+lovelymandal16@users.noreply.github.com>
2025-06-04 15:42:35 +0530

    Update form-editor-support.js

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -398,7 +398,7 @@ function enableAuthoringAssistantExtension() {
 //   meta.content =   'https://experience-stage.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-554-c999b31b5b112fabe552b301eec1e2ad80d34cf7';
 // }
 // else{
-  meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-556-1637a032dc71faf73ee5daca66a8aad333cf08b0';
+  meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-556-f36879e155f01b2925f0a8bf9a31aef049bbeb79';
 // }
  
 
